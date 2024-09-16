1e1391f7a789068adaa1220c6efcc8716bfbc826
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-09-17 00:50:28 +0300

    User validation

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
 
 class ApiController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validateUser = Validator::make($request->all(),
+        [
+            'name' => ['required','string','max:255'],
+            'email' => ['required','string','email','max:255','unique:users'],
+            'password' => ['required','string','min:8','confirmed'],
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
 
+// register route
+Route::post("register",[ApiController::class,'register']);
+
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
